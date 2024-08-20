d40ca49014916d695b8409414a34eb175a8c7e81
sebastianaravenasandoval <sebastianignaaravenasandoval@gmail.com>
2024-08-19 22:39:34 -0400

    inicia el proceso de creacion de test para los modulos creados

--- test/test_main.py
+++ test/test_main.py
@@ -9,3 +9,11 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+def test_protected_route_without_token():
+    response = client.get("v1/users/me")
+    assert response.status_code == 404
+
+def test_create_user_invalid_email():
+    response = client.post("/v1/user/create_user", json={"name": "pruebausuario", "lastname": "testprueba","email":"test.prueba@gmail.com","address":"calle falsa123","paswrd":"test2024"})
+    assert response.status_code == 201
